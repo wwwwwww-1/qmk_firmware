d3948f0d438a21e998dc5ebf6f0795d505f5857a
wwwwwww1 <daniel.hill@runbox.com>
2020-04-17 15:45:04 -0400

    Change back to using RSHIFT instead of two LSHIFT

--- keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
+++ keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      OS_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OS_LSFT,
+     OS_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,           KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, OS_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     OS_LCTL, LOWER,   KC_SPC,                    KC_ENT,  RAISE,   OS_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
