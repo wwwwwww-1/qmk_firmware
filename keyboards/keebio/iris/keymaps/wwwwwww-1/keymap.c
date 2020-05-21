f82636e2970deebce0795b96392e8ad573483e17
wwwwwww1 <daniel.hill@runbox.com>
2020-05-20 20:11:38 -0400

    Add tap dance key that activates lgui or symbol layer as a oneshot

--- keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
+++ keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
@@ -4,8 +4,9 @@ bool pass_esc = true;
 
 enum my_layers {
   _WORKMAN = 0,
-  _SYMBOL,
+  _NUMPAD,
   _ARROW,
+  _SYMBOL,
   _ADJUST
 };
 
@@ -14,15 +15,36 @@ enum custom_keycodes {
   ADJUST
 };
 
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+// Tap dance states
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+
+void guisym_finished (qk_tap_dance_state_t *state, void *user_data);
+void guisym_reset (qk_tap_dance_state_t *state, void *user_data);
+
 // Tap dance keys
 enum {
-  TD_NEXT_PREV = 0
+  CT_NEXT_PREV = 0,
+  CT_GUI_SYMBOL
 };
 
 // Tap dance definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_NEXT_PREV] = ACTION_TAP_DANCE_DOUBLE(KC_MNXT, KC_MPRV)
+  [CT_NEXT_PREV] = ACTION_TAP_DANCE_DOUBLE(KC_MNXT, KC_MPRV),
+  [CT_GUI_SYMBOL] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, guisym_finished, guisym_reset, 275)
 };
+
+#define GUI_SYM TD(CT_GUI_SYMBOL)
 
 // Lighting layer definitions
 const rgblight_segment_t PROGMEM my_shift_layer[] = RGBLIGHT_LAYER_SEGMENTS( {5, 1, HSV_RED} );
@@ -46,9 +68,9 @@ const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
 #define OS_LCTL OSM(MOD_LCTL)
 #define OS_RGUI OSM(MOD_RGUI)
 #define OS_RALT OSM(MOD_RALT)
-#define OS_SYM OSL(_SYMBOL)
+#define TG_NUM TG(_NUMPAD)  //temp key, will eventually want to have a key that toggles on double tap, does something else on single tap
 #define TAB_AR LT(_ARROW, KC_TAB)
-#define TD_NXPR TD(TD_NEXT_PREV)
+#define TD_NXPR TD(CT_NEXT_PREV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -60,21 +82,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      TAB_AR,  KC_A,    KC_S,    KC_H,    KC_T,    KC_G,                               KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     OS_SYM,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_ESC,           ADJUST,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+     TG_NUM,  KC_Z,    KC_X,    KC_M,    KC_C,    KC_V,    KC_ESC,           ADJUST,  KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    OS_LCTL, OS_LSFT, KC_SPC,                    KC_ENT,  OS_RGUI, OS_RALT
+                                    OS_LCTL, OS_LSFT, KC_SPC,                    KC_ENT,  GUI_SYM, OS_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
-  [_SYMBOL] = LAYOUT(
+  [_NUMPAD] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, KC_7,    KC_8,    KC_9,    _______, _______,
+     _______, _______, _______, _______, _______, _______,                            _______, KC_7,    KC_8,    KC_9,    KC_EQL,  _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            KC_LBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_EQL,
+     _______, _______, _______, _______, _______, _______,                            _______, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, _______,          _______, KC_RBRC, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_UNDS,
+     _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -89,6 +111,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______, _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______, _______,          _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    _______, _______, _______,                   _______, _______, _______
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
+  [_SYMBOL] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, KC_PLUS, KC_MINS, KC_LBRC,                            KC_RBRC, _______, _______, _______, _______, _______,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     _______, _______, _______, KC_EQL,  KC_UNDS, KC_LCBR, _______,          _______, KC_RCBR, _______, _______, _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -132,6 +168,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           clear_oneshot_mods();
           pass_esc = false;
         }
+        if (layer_state_is(_SYMBOL)) {
+          layer_off(_SYMBOL);
+          pass_esc = false;
+        }
       }
       return pass_esc;
       break;
@@ -158,7 +198,7 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     rgblight_set_layer_state(0, layer_state_cmp(state, _SYMBOL));
-    rgblight_set_layer_state(1, layer_state_cmp(state, _ARROW));
+    rgblight_set_layer_state(1, layer_state_cmp(state, _NUMPAD));
     return state;
 }
 
@@ -173,5 +213,46 @@ void keyboard_post_init_user(void) {
     rgblight_layers = my_rgb_layers;
     set_single_persistent_default_layer(_WORKMAN);
     layer_clear();
+}
+
+int cur_dance (qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (!state->pressed) {
+      return SINGLE_TAP;
+    } else {
+      return SINGLE_HOLD;
+    }
+  } else if (state->count == 2) {
+    return DOUBLE_TAP;
+  }
+  else return 8;
+}
+
+static tap guisym_tap_state = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void guisym_finished (qk_tap_dance_state_t *state, void *user_data) {
+  guisym_tap_state.state = cur_dance(state);
+  switch (guisym_tap_state.state) {
+    case SINGLE_TAP: 
+      set_oneshot_mods(MOD_LGUI);
+      break;
+    case SINGLE_HOLD: 
+      register_code(KC_LGUI);
+      break;
+    case DOUBLE_TAP: 
+      set_oneshot_layer(_SYMBOL, ONESHOT_START);
+      clear_oneshot_layer_state(ONESHOT_PRESSED);
+      break;
+  }
+}
+
+void guisym_reset (qk_tap_dance_state_t *state, void *user_data) {
+  if (guisym_tap_state.state==SINGLE_HOLD) {
+    unregister_code(KC_LGUI);
+  }
+  guisym_tap_state.state = 0;
 }
 
