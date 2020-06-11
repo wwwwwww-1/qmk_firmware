76a21e2b0b4873d48b3bc22fcf0c71202cc7780a
wwwwwww1 <daniel.hill@runbox.com>
2020-06-11 14:10:49 -0400

    Fix single hold tap dance

--- keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
+++ keyboards/keebio/iris/keymaps/wwwwwww-1/keymap.c
@@ -217,7 +217,7 @@ void keyboard_post_init_user(void) {
 
 int cur_dance (qk_tap_dance_state_t *state) {
   if (state->count == 1) {
-    if (state->interrupted || !state->pressed) {
+    if (!state->pressed) {
       return SINGLE_TAP;
     } else {
       return SINGLE_HOLD;
